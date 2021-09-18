e27ac6a9ee9facbdda3c11b0b30264eb6166ca26
Joseph Ogbonna <58666630+Ogbonna-Joseph@users.noreply.github.com>
2021-09-18 11:20:32 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// document.getElementById("count-el").innerText = 30
+let inputbtn = document.getElementById("input-btn")
+let mylead = []
+const inputEl = document.getElementById("input-el")
 
 
-let countEl = document.getElementById("count-el")
-
-let count = 0
-
-function increment() {
-    count = count + 1
-    countEl.innerText = count 
-    console.log(count)
-}
-
-function Cancel() {
-    let cancel1 = document.getElementById("count-el")
-    cancel1.innerText = 0
-    count = 0
-
-}
-
+inputbtn.addEventListener("click", function(){
+    mylead.push(inputEl.value)
+    console.log(mylead)
+})
